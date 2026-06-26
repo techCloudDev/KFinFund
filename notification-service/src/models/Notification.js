11270d7a0b575e7bef5af29d231ceb4390e72592
techCloudDev <kfinfund@gmail.com>
2026-06-26 23:28:08 +0530

    Refactor Notification schema to use String for userId

--- notification-service/src/models/Notification.js
+++ notification-service/src/models/Notification.js
@@ -1,39 +1,36 @@
 const mongoose = require("mongoose");
 
 const notificationSchema = new mongoose.Schema(
-    {
-        userId: {
-            type: Number,
-            required: true
-        },
-        email: {
-            type: String,
-            required: true
-        },
-        title: {
-            type: String,
-            required: true
-        },
-        message: {
-            type: String,
-            required: true
-        },
-        type: {
-            type: String,
-            enum: ["SIP", "PURCHASE", "ALERT","SYSTEM","KYC"],
-            default: "ALERT"
-        },
-        isRead: {
-            type: Boolean,
-            default: false
-        }
+  {
+    userId: {
+      type: String,
+      required: true
     },
-    {
-        timestamps: true
+    email: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    message: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      enum: ["SIP", "PURCHASE", "ALERT", "SYSTEM", "KYC"],
+      default: "ALERT"
+    },
+    isRead: {
+      type: Boolean,
+      default: false
     }
+  },
+  {
+    timestamps: true
+  }
 );
 
-module.exports = mongoose.model(
-    "Notification",
-    notificationSchema
-);
+module.exports = mongoose.model("Notification", notificationSchema);
